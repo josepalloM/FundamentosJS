2461b759589e31efaac47d8ab40d99f686d5db20
José Pallo <114111169+josepalloM@users.noreply.github.com>
2022-09-21 17:30:35 -0500

    Variables iniciales
    let, const
    Tipos de datos: number, undefined, symbol, boolean, string, Big Int

---
+++ js/01.js
@@ -0,0 +1,17 @@
+// Variables  let - const
+
+//LET
+let cliente = "José"
+let precioDescuento = "220"
+
+// Reasignar
+cliente = 20
+cliente = true
+
+console.log(cliente)
+console.log(precioDescuento)
+
+// CONST
+//	La sintaxis de creación de variables es igual
+// 	No se puede reasignar
+// No pueden iniciar sin ningún valor
---
+++ js/02.js
@@ -0,0 +1,50 @@
+// Tipos de datos
+
+//Undefined
+// let cliente = 20
+// console.log(cliente)
+// console.log(typeof cliente)
+
+//Boolean
+// const descuento= true
+// console.log(descuento)
+// console.log(typeof descuento)
+
+//Number
+// const numero1 = 20.20
+// const numero2 = 30
+// const numero3 = -100
+//
+// console.log(typeof numero1)
+// console.log(typeof numero2)
+// console.log(typeof numero3)
+
+//String
+// const alumno = "Jose"
+// let producto = 'Monitor de 20 pulgadas'
+//
+// const numero = "30"
+// const numero = 30
+
+//Big Int
+// const numeroGrande = BigInt(132132132132123132123323123123312231123)
+//
+// console.log(typeof numeroGrande)
+
+// const numero = "30"
+// const numero2 = 30
+//
+// console.log(typeof String(numero2))
+// console.log(typeof Number(numero))
+
+//Symbol
+// const primerSymbol = Symbol(30)
+// const segundoSymbol = Symbol(30)
+//
+// console.log(primerSymbol === segundoSymbol)
+// console.log(primerSymbol.valueOf())
+// console.log(segundoSymbol.valueOf())
+
+//Null
+const descuento = null
+console.log(typeof descuento)
